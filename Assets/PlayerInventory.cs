2847159c0ca731f29aecfe69a8124ec744d1c5fe
Justin Herring <jmherring4747@gmail.com>
2022-04-02 22:03:54 -0500

    added bread, new start screen

---
+++ Assets/PlayerInventory.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour, IInventory
+{
+    public int numBread { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+}
